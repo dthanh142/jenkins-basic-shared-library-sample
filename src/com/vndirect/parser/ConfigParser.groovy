46078bfa2667eed1188b04a3555384ed10060934
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-24 15:34:59 +0700

    f

--- src/com/vndirect/parser/ConfigParser.groovy
+++ src/com/vndirect/parser/ConfigParser.groovy
@@ -12,15 +12,6 @@ class ConfigParser {
         // parse the environment variables
         projectConfiguration.environment    = parseEnvironment(yaml.template.language);
 
-        // parse the execution steps
-        // projectConfiguration.steps          = parseSteps(yaml.steps);
-
-        // parse the necessary services
-        // projectConfiguration.services   = parseServices(yaml.services);
-
-        // load the dockefile
-        // projectConfiguration.dockerfile = parseDockerfile(yaml.config);
-
         // load the project name
         projectConfiguration.projectName = parseProjectName(yaml.name);
 
@@ -35,59 +26,11 @@ class ConfigParser {
         return environment.collect { k, v -> "${k}=${v}"};
     }
 
-    // static def parseSteps(def yamlSteps) {
-    //     List<Step> steps = yamlSteps.collect { k, v ->
-    //         Step step = new Step(name: k)
-
-    //         // a step can have one or more commands to execute    
-    //         v.each {
-    //             step.commands.add(it);
-    //         }
-    //         return step
-    //     }
-    //     return new Steps(steps: steps);
-    // }
-
-    // static def parseServices(def steps) {
-    //     def services = [];
-
-    //     steps.each {
-    //         def instance = getServiceClass(it.capitalize())?.newInstance()
-    //         services.add(instance)
-    //     };
-
-    //     services.add(new Base());
-
-    //     return services
-    // }
-
-    // static def getServiceClass(def name) {
-    //     switch(name) {
-    //         case "Postgres":
-    //             return Postgres
-    //             break
-    //         case "Redis":
-    //             return Redis
-    //             break
-    //         case "Mssql":
-    //             return Mssql
-    //             break
-    //     }
-    // }
-
-    // static def parseDockerfile(def config) {
-    //     if (!config || !config["dockerfile"]) {
-    //         return "Dockerfile";
-    //     }
-
-    //     return config["dockerfile"];
-    // }
-
     static def parseProjectName(def config) {
-        if (!config || !config["project_name"]) {
+        if (!config || !config["name"]) {
             return "woloxci-project";
         }
 
-        return config["project_name"];
+        return config["name"];
     }
 }
