91a4a32c66482b5ee389436eb393ba289942aeea
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-24 11:52:26 +0700

    fx

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -6,7 +6,7 @@ def call(a) {
                 from: 'jenkins-noreply@vndirect.com.vn',
                 subject: "Please approve ${a} project to production", mimeType: 'text/html',
                 body: """Please <a href="${env.JOB_URL}${env.BUILD_ID}/input/">approve me</a>!"""
-        input message: "Approve ${a} to production", submitter: "thanh.phamduc"
+        input message: "Approve ${a} to production", submitter: "admin,thanh.phamduc"
         // approvalMap = input id: 'test', message: 'Hello', ok: 'Proceed?', parameters: [choice(choices: 'uat\nstag\nprod', description: 'Select a environment for this build', name: 'ENV'), string(defaultValue: '', description: '', name: 'myparam')], submitter: 'thanh.phamduc,user2,group1', submitterParameter: 'APPROVER'
     }
 }
