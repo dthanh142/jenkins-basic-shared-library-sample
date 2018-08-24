7ede88f2c00c853a818b26dbb799ee1191a5ad70
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-24 15:11:46 +0700

    test wolox

---
+++ src/com/vndirect/ProjectConfiguration.groovy
@@ -0,0 +1,12 @@
+package com.vndirect;
+
+import com.vndirect.*;
+
+class ProjectConfig {
+    def environment;
+    // def services;
+    // Steps steps;
+    // def dockerfile;
+    def projectName;
+    def buildNumber;
+}
