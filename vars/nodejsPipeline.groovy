f2bacf0af312f61c5990ccfc29ff8e10749f5a1e
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-27 17:03:26 +0700

    f

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -18,7 +18,7 @@ ADD . /opt/${config.projectName}
 #VOLUME ["/var/log/${config.projectName}","/opt/${config.projectName}"]
 RUN npm i -g pushstate-server
 EXPOSE ${config.port}
-CMD ["${config.runCommand.each}"]"""
+CMD [${config.runCommand}]"""
 
         // dockerBuild(config)
     }
