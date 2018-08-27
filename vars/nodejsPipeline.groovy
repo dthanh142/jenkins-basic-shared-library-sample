c5e720c2b42add02b3f205832ab8a7866e21dd91
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-27 17:43:59 +0700

    f

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -9,6 +9,7 @@ def call(config) {
     // }
     stage("Build docker"){
 
+        String command = String.join(",", ${config.runCommand})
         // Write dockerfile
         writeFile file: 'Dockerfile', text: """FROM node:${config.version}
 WORKDIR /opt/${config.projectName}
@@ -16,7 +17,7 @@ ADD . /opt/${config.projectName}
 #VOLUME ["/var/log/${config.projectName}","/opt/${config.projectName}"]
 RUN npm i -g pushstate-server
 EXPOSE ${config.port}
-CMD ${config.runCommand}"""
+CMD ${command}"""
 
         // dockerBuild(config)
     }
