1dea205138392ae156c2bd25b8bf991bf672d0e0
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-27 11:28:05 +0700

    fx

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -3,6 +3,7 @@ def call(config) {
     switch(config.framework) {
         case 'flask':
             echo "Building flask"
+            vnds.build()
             break
         case 'celery':
             echo "Building celery"
