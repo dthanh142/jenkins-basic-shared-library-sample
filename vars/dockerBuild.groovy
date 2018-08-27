e9a50640b147e61fbd8f7e74dee0f124c7fd625f
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-27 15:17:27 +0700

    f

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -12,8 +12,8 @@ CMD ["pushstate-server","build","9000"]"""
 
     // build and push docker image
     withDockerRegistry([credentialsId: 'docker-nexus-credentials', url: "https://repo.vndirect.com.vn"]) {
-		    def image = docker.build("repo.vndirect.com.vn/${config.projectName}/${BRANCH_NAME}:${GIT_COMMIT}", ".")
-            	image.push("${GIT_COMMIT}")
+		    def image = docker.build("repo.vndirect.com.vn/${config.projectName}/${env.BRANCH_NAME}:${env.GIT_COMMIT}", ".")
+            	image.push("${env.GIT_COMMIT}")
             	image.push('latest')
           }
 }
