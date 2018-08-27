66f1c681d83ce1545953222dbb9926bb2c32516f
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-08-27 18:53:25 +0700

    f

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -12,7 +12,7 @@ def call(config) {
         // def String command = String.join(",", config.runCommand)
         String command = (String) config.runCommand
         println command
-        def command = config.runCommand.toString().split().collect {"\"" +  it.trim() + "\"" }.join(",")
+        // def command = config.runCommand.toString().split().collect {"\"" +  it.trim() + "\"" }.join(",")
     
         // Write dockerfile
         writeFile file: 'Dockerfile', text: """FROM node:${config.version}
