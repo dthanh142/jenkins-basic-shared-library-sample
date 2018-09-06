c36dd6e5dd567774b13e54c7755b6c000e1b1170
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-09-06 11:29:39 +0700

    f

--- vars/testWolox.groovy
+++ vars/testWolox.groovy
@@ -29,9 +29,9 @@ def call() {
         case ['nodejs','node']:
             nodejsPipeline(projectConfig)
             break
-        // default: 
-        //     println "No framework declared"
-        //     break
+        default: 
+            // println "No framework declared"
+            break
     }
     throw new Exception ("No framework declared")
 
