338755bde981263bf11e15dff4ef78b167cd837a
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-09-07 10:26:19 +0700

    f

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -67,7 +67,7 @@ services:
 
         def composeFile = readYaml file: "docker-compose-default.yml"
         println composeFile
-        composeFile.services.'test-jenkins'.volumes = ["/opt/config:/opt/config"]
+        composeFile.services."${config.projectName}".volumes = ["/opt/config:/opt/config"]
         println composeFile
 
         sh "rm docker-compose-default.yml"
