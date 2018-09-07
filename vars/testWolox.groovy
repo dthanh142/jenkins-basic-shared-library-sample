4c41022efcaa20fb9ca0ad41626fcbda03d379aa
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-09-07 15:55:21 +0700

    f

--- vars/testWolox.groovy
+++ vars/testWolox.groovy
@@ -9,7 +9,6 @@ def call() {
     // Map configParser = parser.load(new File(pwd() + '/devops.yaml').text)
     // yaml = configParser
     
-    // echo "${yaml}"
     def yaml = readYaml file: "./devops.yaml";
     println yaml
     def buildNumber = Integer.parseInt(env.BUILD_ID)
@@ -19,6 +18,7 @@ def call() {
     // load project's configuration
     ProjectConfiguration projectConfig = ConfigParser.parse(yaml, buildTag);
     
+    // filter framework
     switch(projectConfig.language.toLowerCase()) {
         case 'python':
             pythonPipeline(projectConfig)
@@ -30,25 +30,27 @@ def call() {
             nodejsPipeline(projectConfig)
             break
         default: 
-            // println "No framework declared"
             throw new Exception ("No framework declared")
             break
     }
 
+    // clean up docker images
     try {
-            def firstImage = sh(
-                script: "docker images --filter 'reference=${projectConfig.projectName}:*' --format \"{{.Tag}}\" | sort -n | head -1",
-                returnStdout: true
-            );
-            firstImage = Integer.parseInt(firstImage.trim());
-            println firstImage
-            for(int i = firstImage; i < buildNumber; i++) {
-                try {
-                    sh "docker images --filter 'reference=${projectConfig.projectName}:${i}' -q | xargs --no-run-if-empty docker rmi -f"
-                } catch(ignored) {
-                    println ignored
-                }
-            }
+            sh "docker images --filter 'reference=repo.vndirect.com.vn/${projectConfig.projectName}/${env.BRANCH_NAME}:${config.buildTag}' -q | xargs --no-run-if-empty docker rmi -f"
+
+            // def firstImage = sh(
+            //     script: "docker images --filter 'reference=${projectConfig.projectName}:*' --format \"{{.Tag}}\" | sort -n | head -1",
+            //     returnStdout: true
+            // );
+            // firstImage = Integer.parseInt(firstImage.trim());
+            // println firstImage
+            // for(int i = firstImage; i < buildNumber; i++) {
+            //     try {
+            //         sh "docker images --filter 'reference=repo.vndirect.com.vn/${projectConfig.projectName}/${env.BRANCH_NAME}:${config.buildTag}' -q | xargs --no-run-if-empty docker rmi -f"
+            //     } catch(ignored) {
+            //         println ignored
+            //     }
+            // }
         } catch(ignored) {
             println ignored
         }
