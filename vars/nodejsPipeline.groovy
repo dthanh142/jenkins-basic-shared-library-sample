59cf95e71cc3a35de01ea3a76ad1b6432b8ed1f6
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-09-07 11:31:56 +0700

    f

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -66,7 +66,7 @@ services:
         def composeFile = readYaml file: "docker-compose-default.yml"
 
         config.configFiles.each { 
-            composeFile.services."${config.projectName}".volumes.plus(it)
+            composeFile.services."${config.projectName}".volumes.add(it)
         }
         
 
