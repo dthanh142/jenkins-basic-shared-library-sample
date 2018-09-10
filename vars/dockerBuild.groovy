165041139dfc7e353777c2d091cbb438dd10feb7
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-09-10 17:15:54 +0700

    comment out push commit tag

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,7 +4,7 @@ def call(config) {
     // build and push docker image
     withDockerRegistry([credentialsId: 'docker-nexus-credentials', url: "https://repo.vndirect.com.vn"]) {
 		    def image = docker.build("repo.vndirect.com.vn/${config.projectName}/${env.BRANCH_NAME}:${config.buildTag}", "--file ${config.dockerfile} .")
-            	image.push("${config.buildTag}")
+            	// image.push("${config.buildTag}")
             	image.push('latest')
           }
 
