f8750808811a3e62ef75bb21d6d5bb24a5a1dcb1
dthanh142 <thanh.phamduc@vndirect.com.vn>
2018-09-11 17:28:17 +0700

    F

--- vars/createDockerCompose.groovy
+++ vars/createDockerCompose.groovy
@@ -32,7 +32,7 @@ services:
       strategy: recreate
       reinitializing_timeout: 60000
   ${config.projectName}:
-    image: repo.vndirect.com.vn/${config.projectName}/${env.BRANCH_NAME}:latest
+    image: repo.vndirect.com.vn/${config.projectName}/${env.BRANCH_NAME}:${projectConfig.buildTag}
     stdin_open: true
     tty: true
     labels:
--- vars/deployUAT.groovy
+++ vars/deployUAT.groovy
@@ -1,5 +1,6 @@
 
 def call(config){
-
+   
+    // deploy to UAT using rancher CLI
     sh "rancher up --pull -d -s ${config.projectName} --upgrade --confirm-upgrade -f ${config.dockerCompose}"
 }
