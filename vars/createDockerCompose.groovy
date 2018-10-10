c1f36555956d3a714d1a155f0c831cb2512126d0
Thanh Pham Duc (DevOps-HO) <thanh.phamduc@vndirect.com.vn>
2018-10-10 10:13:56 +0700

    check type

--- vars/createDockerCompose.groovy
+++ vars/createDockerCompose.groovy
@@ -58,7 +58,7 @@ services:
         composeFile.services."${config.projectName}"["depends_on"] = "redis"
         composeFile.services."${config.projectName}"["command"] = "${config.runCommand}"
 
-        def celery_redis = {
+        def celery_redis = '{
           celery:
             image: "repo.vndirect.com.vn/${config.projectName}/${env.BRANCH_NAME}:${config.buildTag}"
             command: ${config.startCelery}
@@ -66,7 +66,8 @@ services:
               - redis
           redis:
             image: redis
-        }
+        }'
+
         print celery_redis.getClass()
         composeFile.services.add(celery_redis)
     }
