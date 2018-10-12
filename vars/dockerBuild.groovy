eeb20511e69429fa5795dd2a85d120502f325bea
Thanh Pham Duc (DevOps-HO) <thanh.phamduc@vndirect.com.vn>
2018-10-12 16:14:54 +0700

    print branch name

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,6 +3,8 @@ def call(config) {
 
     // build and push docker image
     def env.BRANCH_NAME = env.BRANCH_NAME.toLowerCase()
+    println env.BRANCH_NAME
+
     withDockerRegistry([credentialsId: 'docker-nexus-credentials', url: "https://repo.vndirect.com.vn"]) {
 		    def image = docker.build("repo.vndirect.com.vn/${config.projectName}/${env.BRANCH_NAME}:${config.buildTag}", "--file ${config.dockerfile} .")
             	image.push("${config.buildTag}")
