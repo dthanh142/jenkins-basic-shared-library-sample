ffba37a48e770d93940681c0b8388f652dd93598
Thanh Pham Duc (DevOps-HO) <thanh.phamduc@vndirect.com.vn>
2018-10-25 15:44:24 +0700

    fix branch name uppercase

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,11 @@
 
 def call(config) {
 
+    def BRANCH_NAME = env.BRANCH_NAME.toLowerCase()
     // build and push docker image
 
     withDockerRegistry([credentialsId: 'docker-nexus-credentials', url: "https://repo.vndirect.com.vn"]) {
-		    def image = docker.build("repo.vndirect.com.vn/${config.projectName}/${env.BRANCH_NAME}:${config.buildTag}", "--file ${config.dockerfile} .")
+		    def image = docker.build("repo.vndirect.com.vn/${config.projectName}/${BRANCH_NAME}:${config.buildTag}", "--file ${config.dockerfile} .")
             	image.push("${config.buildTag}")
             	image.push('latest')
           }
